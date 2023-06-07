0f83bd19baf2b49dbbf62de02dd306ea08a28e6e
Klusak <225106@student.uek.krakow.pl>
2023-06-07 14:29:24 +0200

    2023-06-07 AK

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,25 @@
 from app import app
+from flask import render_template, request, redirect, url_for
 
-@app.route("/")
 @app.route("/index")
-
 def index():
-    return "Hello World!"
+    return render_template("index.html")
 
-@app.route("/name", defaults={'name': "Anonim"})
-@app.route("/name/<name>")
-def name(name):
-    return f"Hello {name}!"
+@app.route("/extract")
+def extract():
+    if request.method=="POST":
+        product_code=request.form.get("product_code")
+        return redirect(url_for("product", code=product_code))
+    return render_template("extract.html")
+
+@app.route("/product/<code>")
+def product(code):
+    return render_template("product.html", product_code=code)
+
+@app.route("/products")     
+def products():
+    return render_template("products.html")
+
+@app.route("/author")     
+def author():
+    return render_template("author.html")
